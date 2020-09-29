41bf7982b2d14c723f320d98f1e934f670861436
jfgreffier <jf.greffier@gmail.com>
2020-09-29 21:40:08 +0200

    Minimalist ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 9
+  },
+  "rules": {
+  }
+};
