442e4ccc05e616a6b76bb3cbcb3af6ade6d9ad49
jfgreffier <jf.greffier@gmail.com>
2020-10-05 23:03:48 +0200

    Serve page content from express

--- test/selectors-relative.spec.js
+++ test/selectors-relative.spec.js
@@ -1,4 +1,7 @@
+const Promise = require('promise');
+const express = require('express');
 const { selectors, firefox } = require('playwright');
+let server, port;
 let browser;
 let page;
 
@@ -7,26 +10,25 @@ beforeAll(async () => {
   browser = await firefox.launch();
   page = await browser.newPage();
   await selectors.register('relative', require('../index'));
-  await page.setContent(`
-    <div>
-      <button class="left">Click me</button><span>Middle</span><button class="right">Click me</button>
-    </div>
-    <script>
-      document.querySelector('.left').addEventListener('click', e => {
-        result = 'Left';
-      }, false);
-      document.querySelector('.right').addEventListener('click', e => {
-        result = 'Right';
-      }, false);
-    </script>
-  `);
+
+  const app = express();
+  app.use(express.static('test/assets'))
+  await new Promise((resolve) => {
+    server = app.listen(0, () => {
+      port = server.address().port;
+      console.log(`Running server on ${port}...`);
+      resolve();
+    });
+  });
 });
 
 afterAll(async () => {
+  server.close();
   await browser.close();
 });
 
 it('should use element toRightOf other selector', async () => {
+  await page.goto('http://localhost:' + port);
   await page.click(`text="Click me" >> relative=toRightOf Middle`);
   expect(await page.evaluate('result')).toBe('Right');
 });
