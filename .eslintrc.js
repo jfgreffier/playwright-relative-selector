8975138c09077721d0ab555fb7210427d53efc2d
jfgreffier <jf.greffier@gmail.com>
2020-10-08 17:12:56 +0200

    ESLint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "node": true,
     "jest": true
   },
-  "extends": "eslint:recommended",
+  "extends": ["eslint:recommended", "plugin:prettier/recommended"],
   "parserOptions": {
     "ecmaVersion": 9
   },
