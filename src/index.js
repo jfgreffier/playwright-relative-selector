02b7bbb128b05939b20209291a97dd2f8b06248d
jfgreffier <jf.greffier@gmail.com>
2020-10-16 22:04:56 +0200

    Code splitting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-function above(candidateRect, refRect) {
-  return refRect.y < candidateRect.y + candidateRect.height;
-}
-
-function below(candidateRect, refRect) {
-  return candidateRect.y > refRect.y + refRect.height;
-}
-
-function toLeftOf(candidateRect, refRect) {
-  return refRect.x < candidateRect.x + candidateRect.width;
-}
-
-function toRightOf(candidateRect, refRect) {
-  return candidateRect.x > refRect.x + refRect.width;
-}
-
-function near() {
-  return false;
-}
+const { above, below, near, toLeftOf, toRightOf } = require("./logic");
 
 function meetsProximitySelector(
   candidateBoundingBox,
