0a396985da1b5554dee3a38815b85ca2b2342411
jfgreffier <jf.greffier@gmail.com>
2020-10-20 22:10:38 +0200

    near

--- src/logic.js
+++ src/logic.js
@@ -14,8 +14,13 @@ function toRightOf(candidateRect, refRect) {
   return candidateRect.x >= refRect.x + refRect.width;
 }
 
-function near() {
-  return false;
+function near(candidateRect, refRect, offset = 30) {
+  return (
+    candidateRect.x + candidateRect.width > refRect.x - offset &&
+    candidateRect.x < refRect.x + offset + refRect.width &&
+    candidateRect.y + candidateRect.height > refRect.y - offset &&
+    candidateRect.y < refRect.y + offset + refRect.height
+  );
 }
 
 module.exports = {
